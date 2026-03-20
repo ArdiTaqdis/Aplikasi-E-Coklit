ee62915dea207189f91a8a7c3f75e833a5e02836
ardiaqwami <ardiqolbaedu@gmail.com>
2026-03-20 19:10:41 +0700

    fhgfgh

--- kk.js
+++ kk.js
@@ -637,19 +637,19 @@ if (!localStorage.getItem("userSession") && app) {
    * AUTO SYNC KK → ANGGOTA
    * ===================== */
 
-  function syncKepalaToAnggota() {
+  window.syncKepalaToAnggota = function () {
     const noKK = document.getElementById("kk_no")?.value;
     const nik = document.getElementById("kk_nik")?.value;
     const nama = document.getElementById("kk_nama")?.value;
 
-    const b_noKK = document.getElementById("b_noKK");
-    const b_nik = document.getElementById("b_nik");
-    const b_nama = document.getElementById("b_nama");
+    const b1 = document.getElementById("b_noKK");
+    const b2 = document.getElementById("b_nik");
+    const b3 = document.getElementById("b_nama");
 
-    if (b_noKK) b_noKK.value = noKK || "";
-    if (b_nik) b_nik.value = nik || "";
-    if (b_nama) b_nama.value = nama || "";
-  }
+    if (b1) b1.value = noKK || "";
+    if (b2) b2.value = nik || "";
+    if (b3) b3.value = nama || "";
+  };
 
   /* =====================
    * REGISTER EVENT
