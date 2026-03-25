5f4e839f7298229708ca6375c4a0f0d7da191c13
ardiaqwami <ardiqolbaedu@gmail.com>
2026-03-25 10:00:30 +0700

    create und pdf

--- tervalidasi.js
+++ tervalidasi.js
@@ -39,9 +39,15 @@ function loadTervalidasi() {
               Detail
             </button>
 
+            <button onclick='exportPDF(${JSON.stringify(a)})'>
+              📄 PDF
+            </button>
+
             <button onclick="cetakKK('${a["NO KK"]}')">
               📄 Cetak 1 KK
             </button>
+
+            
           </td>
         </tr>`;
       });
